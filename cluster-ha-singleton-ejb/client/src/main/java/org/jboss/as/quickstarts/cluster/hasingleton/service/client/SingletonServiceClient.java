jboss-developer/jboss-eap-quickstarts

Initial version of quickstart cluster-ha-singleton-ejb

cluster-ha-singleton-ejb/client/src/main/java/org/jboss/as/quickstarts/cluster/hasingleton/service/client/SingletonServiceClient.java
@@ -1,0 +1,72 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the 
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.as.quickstarts.cluster.hasingleton.service.client;
+
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.jboss.as.quickstarts.cluster.hasingleton.service.ejb.ServiceAccess;
+
+/**
+ * A client to call the SingletonService via EJB remoting (AS7) to demonstrate the behaviour of the singleton.
+ * 
+ * @author <a href="mailto:wfink@redhat.com">Wolf-Dieter Fink</a>
+ */
+public class SingletonServiceClient {
+    /**
+     * Proxy of the SLSB
+     */
+    private final ServiceAccess accessBean;
+
+    /**
+     * Constructor to prepare the client-context.<br/>
+     * There must be a jboss-ejb-client.properties file in the classpath to specify the server connection(s).
+     * 
+     * @throws NamingException
+     */
+    private SingletonServiceClient() throws NamingException {
+        final Hashtable<String, String> jndiProperties = new Hashtable<String, String>();
+        jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+        final Context context = new InitialContext(jndiProperties);
+        String lookupName = "ejb:/jboss-as-cluster-ha-singleton-ejb//ServiceAccessBean!" + ServiceAccess.class.getName();
+        System.out.println("Lookup Bean >" + lookupName);
+        accessBean = (ServiceAccess) context.lookup(lookupName);
+    }
+
+    private String getServiceNodeName() {
+        return accessBean.getNodeNameOfService();
+    }
+
+    /**
+     * Call the EJB 4 times to demonstrate that despite loadbalancing the SingletonService is called at the same server.
+     * 
+     * @param args no arguments needed
+     * @throws NamingException
+     */
+    public static void main(String[] args) throws NamingException {
+        SingletonServiceClient client = new SingletonServiceClient();
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("#" + i + " The service is active on node with name = " + client.getServiceNodeName());
+        }
+
+    }
+
+}

cluster-ha-singleton-ejb/service/src/main/java/org/jboss/as/quickstarts/cluster/hasingleton/service/ejb/ServiceAccess.java
@@ -1,0 +1,29 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the 
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.as.quickstarts.cluster.hasingleton.service.ejb;
+
+import javax.ejb.Remote;
+
+/**
+ * Business interface to access the SingletonService via this EJB
+ * 
+ * @author <a href="mailto:wfink@redhat.com">Wolf-Dieter Fink</a>
+ */
+@Remote
+public interface ServiceAccess {
+    public abstract String getNodeNameOfService();
+}

cluster-ha-singleton-ejb/service/src/main/java/org/jboss/as/quickstarts/cluster/hasingleton/service/ejb/ServiceAccessBean.java
@@ -1,0 +1,46 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the 
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.as.quickstarts.cluster.hasingleton.service.ejb;
+
+import javax.ejb.Stateless;
+
+import org.jboss.as.server.CurrentServiceContainer;
+import org.jboss.msc.service.ServiceController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A simple SLSB to access the internal SingletonService.
+ * 
+ * @author <a href="mailto:wfink@redhat.com">Wolf-Dieter Fink</a>
+ */
+@Stateless
+public class ServiceAccessBean implements ServiceAccess {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceAccessBean.class);
+
+    public String getNodeNameOfService() {
+        LOGGER.info("getNodeNameOfService() is called()");
+        ServiceController<?> service = CurrentServiceContainer.getServiceContainer().getService(
+                EnvironmentService.SINGLETON_SERVICE_NAME);
+        LOGGER.debug("SERVICE {}", service);
+        if (service != null) {
+            return (String) service.getValue();
+        } else {
+            throw new IllegalStateException("Service '" + EnvironmentService.SINGLETON_SERVICE_NAME + "' not found!");
+        }
+    }
+}

cluster-ha-singleton-ejb/service/src/main/java/org/jboss/as/quickstarts/cluster/hasingleton/service/ejb/StartupSingleton.java
@@ -1,0 +1,124 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the 
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.as.quickstarts.cluster.hasingleton.service.ejb;
+
+import java.util.Collection;
+import java.util.EnumSet;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+import org.jboss.as.clustering.singleton.SingletonService;
+import org.jboss.as.server.CurrentServiceContainer;
+import org.jboss.as.server.ServerEnvironment;
+import org.jboss.as.server.ServerEnvironmentService;
+import org.jboss.msc.service.AbstractServiceListener;
+import org.jboss.msc.service.ServiceController;
+import org.jboss.msc.service.ServiceController.Transition;
+import org.jboss.msc.service.ServiceListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * A Singleton EJB to create the SingletonService during startup.
+ * 
+ * @author <a href="mailto:wfink@redhat.com">Wolf-Dieter Fink</a>
+ */
+@Singleton
+@Startup
+public class StartupSingleton {
+	private static final Logger LOGGER = LoggerFactory.getLogger(StartupSingleton.class);
+
+	/**
+	 * Create the Service and wait until it is started.<br/>
+	 * Will log a message if the service will not start in 10sec. 
+	 */
+	@PostConstruct
+	protected void startup() {
+		LOGGER.info("StartupSingleton will be initialized!");
+
+		EnvironmentService service = new EnvironmentService();
+		SingletonService<String> singleton = new SingletonService<String>(service, EnvironmentService.SINGLETON_SERVICE_NAME);
+		// if there is a node where the Singleton should deployed the election policy might set,
+		// otherwise the JGroups coordinator will start it
+		//singleton.setElectionPolicy(new PreferredSingletonElectionPolicy(new NamePreference("node2/cluster"), new SimpleSingletonElectionPolicy()));
+		ServiceController<String> controller = singleton.build(CurrentServiceContainer.getServiceContainer())
+				.addDependency(ServerEnvironmentService.SERVICE_NAME, ServerEnvironment.class, service.getEnvInjector())
+				.install();
+
+		controller.setMode(ServiceController.Mode.ACTIVE);
+		try {
+			wait(controller, EnumSet.of(ServiceController.State.DOWN, ServiceController.State.STARTING), ServiceController.State.UP);
+			LOGGER.info("StartupSingleton has started the Service");
+		} catch (IllegalStateException e) {
+			LOGGER.warn("Singleton Service {} not started, are you sure to start in a cluster (HA) environment?",EnvironmentService.SINGLETON_SERVICE_NAME);
+		}
+	}
+
+	/**
+	 * Remove the service during undeploy or shutdown
+	 */
+	@PreDestroy
+	protected void destroy() {
+		LOGGER.info("StartupSingleton will be removed!");
+		ServiceController<?> controller = CurrentServiceContainer.getServiceContainer().getRequiredService(EnvironmentService.SINGLETON_SERVICE_NAME);
+		controller.setMode(ServiceController.Mode.REMOVE);
+		try {
+			wait(controller, EnumSet.of(ServiceController.State.UP, ServiceController.State.STOPPING, ServiceController.State.DOWN), ServiceController.State.REMOVED);
+		} catch (IllegalStateException e) {
+			LOGGER.warn("Singleton Service {} has not be stopped correctly!",EnvironmentService.SINGLETON_SERVICE_NAME);
+		}
+	}
+
+	private static <T> void wait(ServiceController<T> controller, Collection<ServiceController.State> expectedStates, ServiceController.State targetState) {
+		if (controller.getState() != targetState) {
+			ServiceListener<T> listener = new NotifyingServiceListener<T>();
+			controller.addListener(listener);
+			try {
+				synchronized (controller) {
+					int maxRetry = 2;
+					while (expectedStates.contains(controller.getState()) && maxRetry > 0) {
+						LOGGER.info("Service controller state is {}, waiting for transition to {}", new Object[] {controller.getState(), targetState});
+						controller.wait(5000);
+						maxRetry--;
+					}
+				}
+			} catch (InterruptedException e) {
+				LOGGER.warn("Wait on startup is interrupted!");
+				Thread.currentThread().interrupt();
+			}
+			controller.removeListener(listener);
+			ServiceController.State state = controller.getState();
+			LOGGER.info("Service controller state is now {}",state);
+			if (state != targetState) {
+				throw new IllegalStateException(String.format("Failed to wait for state to transition to %s.  Current state is %s", targetState, state), controller.getStartException());
+			}
+		}
+	}
+
+	private static class NotifyingServiceListener<T> extends AbstractServiceListener<T> {
+		@Override
+		public void transition(ServiceController<? extends T> controller, Transition transition) {
+			synchronized (controller) {
+				controller.notify();
+			}
+		}
+	}
+}

sanogotech/JBOSSWildflyQuickstart

cluster-ha-singleton-ejb/client/src/main/java/org/jboss/as/quickstarts/cluster/hasingleton/service/client/SingletonServiceClient.java
@@ -1,0 +1,72 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the 
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.as.quickstarts.cluster.hasingleton.service.client;
+
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.jboss.as.quickstarts.cluster.hasingleton.service.ejb.ServiceAccess;
+
+/**
+ * A client to call the SingletonService via EJB remoting (AS7) to demonstrate the behaviour of the singleton.
+ * 
+ * @author <a href="mailto:wfink@redhat.com">Wolf-Dieter Fink</a>
+ */
+public class SingletonServiceClient {
+    /**
+     * Proxy of the SLSB
+     */
+    private final ServiceAccess accessBean;
+
+    /**
+     * Constructor to prepare the client-context.<br/>
+     * There must be a jboss-ejb-client.properties file in the classpath to specify the server connection(s).
+     * 
+     * @throws NamingException
+     */
+    private SingletonServiceClient() throws NamingException {
+        final Hashtable<String, String> jndiProperties = new Hashtable<String, String>();
+        jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+        final Context context = new InitialContext(jndiProperties);
+        String lookupName = "ejb:/jboss-as-cluster-ha-singleton-ejb//ServiceAccessBean!" + ServiceAccess.class.getName();
+        System.out.println("Lookup Bean >" + lookupName);
+        accessBean = (ServiceAccess) context.lookup(lookupName);
+    }
+
+    private String getServiceNodeName() {
+        return accessBean.getNodeNameOfTimerService();
+    }
+
+    /**
+     * Call the EJB 4 times to demonstrate that despite loadbalancing the SingletonService is called at the same server.
+     * 
+     * @param args no arguments needed
+     * @throws NamingException
+     */
+    public static void main(String[] args) throws NamingException {
+        SingletonServiceClient client = new SingletonServiceClient();
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("#" + i + " The service is active on node with name = " + client.getServiceNodeName());
+        }
+
+    }
+
+}

cluster-ha-singleton-ejb/service/src/main/java/org/jboss/as/quickstarts/cluster/hasingleton/service/ejb/ServiceAccess.java
@@ -1,0 +1,34 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the 
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.as.quickstarts.cluster.hasingleton.service.ejb;
+
+import javax.ejb.Remote;
+
+/**
+ * Business interface to access the SingletonService via this EJB
+ * 
+ * @author <a href="mailto:wfink@redhat.com">Wolf-Dieter Fink</a>
+ */
+@Remote
+public interface ServiceAccess {
+    /**
+     * Provide the node name where the scheduler is started.
+     * 
+     * @return name of the cluster node where the schedule timer is running
+     */
+    public abstract String getNodeNameOfTimerService();
+}

cluster-ha-singleton-ejb/service/src/main/java/org/jboss/as/quickstarts/cluster/hasingleton/service/ejb/ServiceAccessBean.java
@@ -1,0 +1,46 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the 
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.as.quickstarts.cluster.hasingleton.service.ejb;
+
+import javax.ejb.Stateless;
+
+import org.jboss.as.server.CurrentServiceContainer;
+import org.jboss.msc.service.ServiceController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A simple SLSB to access the internal SingletonService.
+ * 
+ * @author <a href="mailto:wfink@redhat.com">Wolf-Dieter Fink</a>
+ */
+@Stateless
+public class ServiceAccessBean implements ServiceAccess {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceAccessBean.class);
+
+    public String getNodeNameOfTimerService() {
+        LOGGER.info("getNodeNameOfTimerService() is called()");
+        ServiceController<?> service = CurrentServiceContainer.getServiceContainer().getService(
+                HATimerService.SINGLETON_SERVICE_NAME);
+        LOGGER.debug("SERVICE {}", service);
+        if (service != null) {
+            return (String) service.getValue();
+        } else {
+            throw new IllegalStateException("Service '" + HATimerService.SINGLETON_SERVICE_NAME + "' not found!");
+        }
+    }
+}

cluster-ha-singleton-ejb/service/src/main/java/org/jboss/as/quickstarts/cluster/hasingleton/service/ejb/StartupSingleton.java
@@ -1,0 +1,124 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the 
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.as.quickstarts.cluster.hasingleton.service.ejb;
+
+import java.util.Collection;
+import java.util.EnumSet;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+import org.jboss.as.clustering.singleton.SingletonService;
+import org.jboss.as.server.CurrentServiceContainer;
+import org.jboss.as.server.ServerEnvironment;
+import org.jboss.as.server.ServerEnvironmentService;
+import org.jboss.msc.service.AbstractServiceListener;
+import org.jboss.msc.service.ServiceController;
+import org.jboss.msc.service.ServiceController.Transition;
+import org.jboss.msc.service.ServiceListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * A Singleton EJB to create the SingletonService during startup.
+ * 
+ * @author <a href="mailto:wfink@redhat.com">Wolf-Dieter Fink</a>
+ */
+@Singleton
+@Startup
+public class StartupSingleton {
+	private static final Logger LOGGER = LoggerFactory.getLogger(StartupSingleton.class);
+
+	/**
+	 * Create the Service and wait until it is started.<br/>
+	 * Will log a message if the service will not start in 10sec. 
+	 */
+	@PostConstruct
+	protected void startup() {
+		LOGGER.info("StartupSingleton will be initialized!");
+
+		HATimerService service = new HATimerService();
+		SingletonService<String> singleton = new SingletonService<String>(service, HATimerService.SINGLETON_SERVICE_NAME);
+		// if there is a node where the Singleton should deployed the election policy might set,
+		// otherwise the JGroups coordinator will start it
+		//singleton.setElectionPolicy(new PreferredSingletonElectionPolicy(new NamePreference("node2/cluster"), new SimpleSingletonElectionPolicy()));
+		ServiceController<String> controller = singleton.build(CurrentServiceContainer.getServiceContainer())
+				.addDependency(ServerEnvironmentService.SERVICE_NAME, ServerEnvironment.class, service.env)
+				.install();
+
+		controller.setMode(ServiceController.Mode.ACTIVE);
+		try {
+			wait(controller, EnumSet.of(ServiceController.State.DOWN, ServiceController.State.STARTING), ServiceController.State.UP);
+			LOGGER.info("StartupSingleton has started the Service");
+		} catch (IllegalStateException e) {
+			LOGGER.warn("Singleton Service {} not started, are you sure to start in a cluster (HA) environment?",HATimerService.SINGLETON_SERVICE_NAME);
+		}
+	}
+
+	/**
+	 * Remove the service during undeploy or shutdown
+	 */
+	@PreDestroy
+	protected void destroy() {
+		LOGGER.info("StartupSingleton will be removed!");
+		ServiceController<?> controller = CurrentServiceContainer.getServiceContainer().getRequiredService(HATimerService.SINGLETON_SERVICE_NAME);
+		controller.setMode(ServiceController.Mode.REMOVE);
+		try {
+			wait(controller, EnumSet.of(ServiceController.State.UP, ServiceController.State.STOPPING, ServiceController.State.DOWN), ServiceController.State.REMOVED);
+		} catch (IllegalStateException e) {
+			LOGGER.warn("Singleton Service {} has not be stopped correctly!",HATimerService.SINGLETON_SERVICE_NAME);
+		}
+	}
+
+	private static <T> void wait(ServiceController<T> controller, Collection<ServiceController.State> expectedStates, ServiceController.State targetState) {
+		if (controller.getState() != targetState) {
+			ServiceListener<T> listener = new NotifyingServiceListener<T>();
+			controller.addListener(listener);
+			try {
+				synchronized (controller) {
+					int maxRetry = 2;
+					while (expectedStates.contains(controller.getState()) && maxRetry > 0) {
+						LOGGER.info("Service controller state is {}, waiting for transition to {}", new Object[] {controller.getState(), targetState});
+						controller.wait(5000);
+						maxRetry--;
+					}
+				}
+			} catch (InterruptedException e) {
+				LOGGER.warn("Wait on startup is interrupted!");
+				Thread.currentThread().interrupt();
+			}
+			controller.removeListener(listener);
+			ServiceController.State state = controller.getState();
+			LOGGER.info("Service controller state is now {}",state);
+			if (state != targetState) {
+				throw new IllegalStateException(String.format("Failed to wait for state to transition to %s.  Current state is %s", targetState, state), controller.getStartException());
+			}
+		}
+	}
+
+	private static class NotifyingServiceListener<T> extends AbstractServiceListener<T> {
+		@Override
+		public void transition(ServiceController<? extends T> controller, Transition transition) {
+			synchronized (controller) {
+				controller.notify();
+			}
+		}
+	}
+}